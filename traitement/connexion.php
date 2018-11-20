3f42cfb333918028c6bdecaf69a3a1cfe2f8f3b5
traccoenlea <leatraccoen@gmail.com>
2018-11-20 15:15:06 +0100

    login.php modifié

--- traitement/connexion.php
+++ traitement/connexion.php
@@ -1,26 +1,26 @@
-<?php
-
-$sql = "SELECT * FROM user WHERE login=? AND mdp=PASSWORD(?)";
+<?php if(isset($_POST["login"]) && isset($_POST['password'])) {
+$sql = "SELECT * FROM utilisateur WHERE login=? AND mdp=PASSWORD(?)";
 
 // Etape 1  : preparation
-$query = $pdo->prepare($sql); 
-// Etape 2 : execution : 2 paramètres dans la requêtes !!
-$query->execute(array($_POST['pseudo'], $_POST['mdp']));
+$connexion = $pdo->prepare($sql);
 
+// Etape 2 : execution : 2 paramètres dans la requêtes !!
+
+$connexion->execute(array($_POST["login"], $_POST['password']));
 // Etape 3 : ici le login est unique, donc on sait que l'on peut avoir zero ou une  seule ligne.
 
-
-
 // un seul fetch
-$line = $query->fetch();
+$line = $connexion->fetch();
 
 // Si $line est faux le couple login mdp est mauvais, on retourne au formulaire
-if($line == false){
-	include"vues/login.php";
-	echo "Erreur dans les données fournies";
-} else {
-	$_SESSION['id'] = $line['id']; 
-	$_SESSION['login']= $line['login'];
-	header("Location: index.php");
-}
+if($line == false)
+header("Location: login.php");
+else {
+$_SESSION['id'] = $line['id'];
+$_SESSION['login'] = $line['login'];
+header("Location: index.php");
 // sinon on crée les variables de session $_SESSION['id'] et $_SESSION['login'] et on va à la page d'accueil
+
+}
+}
+?>
