d6333fccb82a4744936d8955600ffed24dc16c63
MrAntoine <antoinevdb15@gmail.com>
2018-11-20 22:37:38 +0100

    Mur + Mes amis

--- vues/friends.php
+++ vues/friends.php
@@ -1,3 +1,62 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Antoine
+ * Date: 20/11/2018
+ * Time: 21:52
+ */
+
+
+$_SESSION["id"] = 1;
+$_SESSION["login"] = "gilles";
+
+
+if(!isset($_SESSION["id"])) {
+    // On n est pas connecté, il faut retourner à la pgae de login
+    header("Location:exempleMur.html?action=login");
+}
+
+
+    // Verifions si on est amis avec cette personne
+    $sql = "SELECT * FROM lien WHERE etat='ami'
+                AND (idUtilisateur1=?)";
+
+    $sql2 = "SELECT login FROM user WHERE (id=?)";
+
+    // Etape 1  : preparation
+        $query = $pdo->prepare($sql);
+        $query2 = $pdo->prepare($sql2);
+
+    // Etape 2 : execution : 2 paramètres dans la requêtes !!
+        $query->execute(array($_SESSION['id']));
+
+
+
+        // Etape 3 :
+        while($line = $query->fetch()) {
+
+            // Requete pour truover le nom de l'ami
+            $query2->execute(array($line["idUtilisateur2"]));
+            $line2= $query2->fetch();
+
+
+            //Affichage
+            echo " <div class='friend margin anim'> ";
+            echo "  <div class='img_article'></div>";
+            echo " <a href='myProfile.php' class='nomPersonne'>";
+            echo "<p>". $line["idUtilisateur2"] ."</p>";
+            echo "<a href='index.php?action=mur&id". $line["idUtilisateur2"] ."'>".$line2["login"]."</a>";
+            echo "</a>";
+
+    }
+?>
+
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
   <head>
@@ -83,48 +142,13 @@
             <input type="submit" name="delFriend" value="Supprimer l'ami">
 
         </div>
-        <div class="friend margin anim">
-            <div class="img_article"></div>
-            <a href="myProfile.php" class="nomPersonne">
-              <p>NOM DE LA PERSONNE</p>
-            </a>
-
-            <input type="submit" name="delFriend" value="Supprimer l'ami">
-
-        </div>
-        <div class="friend margin anim">
-            <div class="img_article"></div>
-            <a href="myProfile.php" class="nomPersonne">
-              <p>NOM DE LA PERSONNE</p>
-            </a>
-
-            <input type="submit" name="delFriend" value="Supprimer l'ami">
-
-        </div>
-        <div class="friend margin anim">
-            <div class="img_article"></div>
-            <a href="myProfile.php" class="nomPersonne">
-              <p>NOM DE LA PERSONNE</p>
-            </a>
-
-            <input type="submit" name="delFriend" value="Supprimer l'ami">
-
-        </div>
-
 
       </div>
     </div>
 
 
 
-
-
-
     <!-- </div> -->
-
-
-
-
 
 
 
