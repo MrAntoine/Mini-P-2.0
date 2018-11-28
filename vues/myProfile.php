d863d6aa621261426c9321496400c3ec9b7e17db
Gautier Theo <theo_gautier@ens.univ-artois.fr>
2018-11-28 21:58:12 +0100

    AJOUT DE PLEIN DE TRUCS

--- vues/myProfile.php
+++ vues/myProfile.php
@@ -1,77 +1,21 @@
-<?php
+<div class="wrapper">
+  <div class="Form form2">
+    <h1>Changez vos TRUCS !</h1>
+    <form method="post" action="">
+        <label for="nom">REQUETE POUR LE NOM</label>
+        <input type="text" id="nom" required name="name" placeholder="Entrez votre nom">
+        <br>
+        <label for="email">REQUETE POUR L'EMAIL</label>
+        <input type="email" id="email" required name="mail-address" placeholder="Entrez votre adresse e-mail">
+        <br>
+        <label for="password">Nouveau mot de passe</label>
+        <input type="password" id="password" required name="password">
+        <br>
 
-/**
- * Created by PhpStorm.
- * User: Antoine
- * Date: 20/11/2018
- * Time: 21:52
- */
-
-
-$_SESSION["id"] = 1;
-$_SESSION["login"] = "gilles";
-
-
-if(!isset($_SESSION["id"])) {
-    // On n est pas connecté, il faut retourner à la pgae de login
-    header("Location:exempleMur.html?action=login");
-}
-
-
-// Verifions si on est amis avec cette personne
-$sql = "SELECT * FROM ecrit WHERE (idAuteur=?)";
-$sql2 = "SELECT login FROM user WHERE (id=?)";
-
-// Etape 1  : preparation
-$query = $pdo->prepare($sql);
-$query2 = $pdo->prepare($sql2);
-// Etape 2 : execution : 2 paramètres dans la requêtes !!
-$query->execute(array($_SESSION['id']));
-
-// Etape 3 :
-while($line = $query->fetch()) {
-
-    // Requete pour trouver le nom de l'ami
-    $query2->execute(array($line["idAuteur"]));
-    $line2= $query2->fetch();
-
-    //Affichage
-
-    echo "<div class='article margin anim'>";
-          echo"<div class='img_article'></div>";
-          echo"<a href='#' class='nomPersonne'>";
-            echo "<p><a href='index.php?action=mur&id". $line["idAuteur"] ."'>".$line2["login"]."</a></p>";
-          echo"</a>";
-          echo"<div class='date_article'>".$line['dateEcrit']."</div>";
-          echo"<div class='article-corps anim'>";
-    echo"<p>".$line['titre']."</p>";
-    echo"<p>".$line['contenu']."</p>";
-          echo"<div class='texte-article'>";
-          echo"</div>";
-          echo"</div>";
-          echo"</div>";
-
-
-}
-?>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        <label for="passwordcf">Confirmez le nouveau mot de passe</label>
+        <input type="password" required name="passwordcf">
+        <input type="submit" name="send" value="Send it !">
+    </form>
+  </div>
+</div>
+</div>
