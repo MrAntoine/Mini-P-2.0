cdb87a9dedb5239dfd48b934730976f692077dad
Gautier Theo <theo_gautier@ens.univ-artois.fr>
2018-12-10 19:19:56 +0100

    Pour Antoine, à repasser en php propre -- MyProfile.php

--- vues/myProfile.php
+++ vues/myProfile.php
@@ -1,12 +1,17 @@
-<?php
-echo '<div class="wrapper">
+<div class="wrapper">
   <div class="FormCGTDATA form2" id="cgt__DATA">
     <h1>Changez vos informations !</h1>
     <form method="post" action="">
-
-        <input type="text" id="nom" required name="name" value="'.$_SESSION['login'] .'">
+        <!-- <label for="nom">Nom actuel :
+          <?php echo $_SESSION['login'];?>
+        </label> -->
+        <input type="text" id="nom" required name="name" value="<?php echo $_SESSION['login'];?>">
+ <!-- placeholder="Votre nouveau nom"  -->
         <br>
-        <input type="email" id="email" required name="mail-address" value="'.$_SESSION['email'].'">
+        <!-- <label for="email">Email actuel :
+          <?php echo $line['email'] ?>
+        </label> -->
+        <input type="email" id="email" required name="mail-address" value="<?php echo $line['email'];?>">
         <br>
         <label for="password">Votre nouveau mot de passe</label>
         <input type="password" id="password" required name="password">
@@ -21,20 +26,24 @@ echo '<div class="wrapper">
 
   </div>
 
-    <div class=\'FormCGTDATA form2\' id=\'cgt__PDP\'>
-    <h1>Changez votre PDP!</h1>
-    <form method=\'post\' action=\'index.php?action=upload\' enctype=\'multipart/form-data\'>
-    <label for=\'actual__img\'>Photo de profil actuelle</label>
-          <img src=\'uploads/" . $line[\'avatar\'] . "\' alt=\'Photo de profil\' class=\'myProfile__img\' id=\'actual__img\'>
-        <br><br>
-        <label for=\'fileToUpload\'>Changez de photo de profil</label>
-        <input type=\'file\' name=\'fileToUpload\'>
+    <?php
 
-        <br><br>
+    echo " <div class='FormCGTDATA form2' id='cgt__PDP'>";
+    echo "<h1>Changez votre PDP!</h1>";
+      echo "<form method='post' action='index.php?action=upload' enctype='multipart/form-data'> ";
+      echo " <label for='actual__img'>Photo de profil actuelle</label>";
+          echo "<img src='uploads/" . $line['avatar'] . "' alt='Photo de profil' class='myProfile__img' id='actual__img'>";
+        echo "<br><br>";
+        echo "<label for='fileToUpload''>Changez de photo de profil</label>";
+          echo "<input type='file' name='fileToUpload'>";
 
-          <input type=\'submit\' name=\'submit_avatar\'></form>
-  </div>
+        echo "<br><br>";
+
+          echo "<input type='submit' name='submit_avatar'></form>";
+
+
+  echo "</div>";
+
+    ?>
 </div>
-</div>';
-
-?>
+</div>
