79696b9effd994a434dee08ea83d55e1becfb35b
Gautier Theo <theo_gautier@ens.univ-artois.fr>
2018-12-18 10:23:43 +0100

    endDiv on userList

--- vues/search_profile.php
+++ vues/search_profile.php
@@ -24,10 +24,11 @@ $query->execute(array($_POST['search_profile']));
 if ($_POST['search_profile'] == "") {
     echo "<p class\"noUserText wrapper\">Désolé mais aucun membre n'est inscrit avec ce pseudo. <br/> Si vous connaissez cette personne vous pouvez lui suggérer de créer un compte.</p>";
 } else {
-    echo "<div class=\"userList wrapper\">Liste des utilisateurs : </div>";
+    echo "<div class=\"userList wrapper\">Liste des utilisateurs : ";
     while ($line = $query->fetch()) {
         echo "<a href='index.php?action=mur&id=" . $line['id'] . "'>" . $line['login'] . "</a><br/>";
     }
+    echo "</div>";
 }
 
 
