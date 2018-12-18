7e4ea504a5b6a4fb4bb757b772a8279cf0f255be
MrAntoine <antoinevdb15@gmail.com>
2018-12-18 10:53:23 +0100

    ajout image par defaut

--- traitement/register.php
+++ traitement/register.php
@@ -1,10 +1,13 @@
 <?php
 
 if(isset($_POST["login"]) && isset($_POST['email']) && isset($_POST['mdp'])) {
-    $sql = "INSERT INTO user(login,mdp,email) VALUES(?,PASSWORD(?),?)";
+    $sql = "INSERT INTO user(login,mdp,email,avatar) VALUES(?,PASSWORD(?),?,?)";
+
+        $avatar = "default-avatar.jpg";
+
 
     $query = $pdo->prepare($sql);
-    $query->execute(array($_POST['login'], $_POST['mdp'], $_POST['email']));
+    $query->execute(array($_POST['login'], $_POST['mdp'], $_POST['email'], $avatar));
     /*$query ->execute(array(
         'login'=> $_POST['login'],
         'mdp' => $_POST['mdp'],
